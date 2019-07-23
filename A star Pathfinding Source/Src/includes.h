669289d35b797516b06fb8a213f23026750d990b
Desperationis <smarttdiego@gmail.com>
2019-07-23 13:04:30 -0700

    Added Libraries and such

---
+++ A star Pathfinding Source/Src/includes.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// standard
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+#include <ctime>
+#include <algorithm>
+#include <memory>
+#include <unordered_map>
+#include <map>
+#include <functional>
+
+
+
+// non-standard
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+#include "tsl/hopscotch_map.h"
